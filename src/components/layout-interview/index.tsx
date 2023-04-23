6229cba63046e16ff0925788aff7821e5623ea2b
cws <2354554898@qq.com>
2023-04-23 14:42:55 +0800

    fix: 修复面试情况前后端切换时数据不更新

--- src/components/layout-interview/index.tsx
+++ src/components/layout-interview/index.tsx
@@ -47,6 +47,7 @@ const LayoutInter: FC<LayoutInterProps> = () => {
       // 将获取过来的面试时间转化为 select组件需要的格式
       await getInterviewTimeDirec(Number(params.direction)).then((res) => {
         const listData = res.data as interviewTime[];
+        //   遍历获取的数据，如果当天有面试则存入其中
         innerTimeList = listData.map((item) => {
           const date = item.startTime;
           if (date.split(' ')[0] === formatDate(new Date()))
@@ -60,40 +61,58 @@ const LayoutInter: FC<LayoutInterProps> = () => {
 
       // 加载数据
       fetchList(innerInterviewTimeId);
-
+      // 设置selector的日期信息
       setDateList(innerTimeList);
+      // 更改selector显示的日期,以确保轮询能正常工作
+      setInterviewTimeId(innerInterviewTimeId);
       setLoading(false);
     }
     fetchData();
   }, [params]);
 
+  // 开启轮询功能，获取数据
   useEffect(() => {
-    const timer = setInterval(() => {
-      fetchList(interviewTimeId, false);
-    }, 4000);
-    return () => {
-      clearInterval(timer);
-    };
+    if (interviewTimeId !== undefined) {
+      const timer = setInterval(() => {
+        console.log('轮询', interviewTimeId);
+
+        fetchList(interviewTimeId, false);
+      }, 4000);
+      return () => {
+        clearInterval(timer);
+      };
+    }
+    // console.log('触发轮询了', interviewTimeId);
   }, [interviewTimeId]);
-  // 更新待面试队列和排队队列
+
+  /**
+   *  更新待面试队列和排队队列
+   * @param value 传递过来的面试时间id
+   * @param forLoading 是否要触发加载动画
+   */
   const fetchList = async (value: number | undefined, forLoading = true) => {
+    // console.log('当前面试时间Id:', interviewTimeId);
+
     if (typeof value === 'number') {
       setInterviewTimeId(value);
       forLoading === true && setLoading(true);
+
       getRecruitTimeInfo(value).then(({ code, data }) => {
         code === 200 ? setQueueList(data.info) : setQueueList([]);
         forLoading === true && setLoading(false);
       });
       // 获取正在面试的队列
       await getIsInterviewByid(value).then((res) => {
-        console.log('正在面试', res);
-
         res.code === 200 ? setIsInterview([res.data]) : setIsInterview([]);
         res.data !== null
           ? (isInterviewId.current = res.data.id)
           : (isInterviewId.current = undefined);
       });
-    } else return;
+    } else {
+      // 如果判断传过来的面试时间id为undefined，则将队列置为空数组
+      setIsInterview([]);
+      setQueueList([]);
+    }
   };
 
   const nextOne = useThrottle(async () => {
@@ -109,10 +128,12 @@ const LayoutInter: FC<LayoutInterProps> = () => {
       fetchList(interviewTimeId);
       return;
     }
+
+    //   发送请求让下一位同学前来面试
     nextInterview(interviewTimeId as number).then((res) => {
       if (res.code === 200) {
         fetchList(interviewTimeId).then(() => {
-          console.log(isInterviewId.current);
+          // 发送消息通知
           isInterviewId.current !== undefined &&
             pushInterview(isInterviewId.current).then((res) => {
               console.log(res);
@@ -133,7 +154,6 @@ const LayoutInter: FC<LayoutInterProps> = () => {
         <span>选择日期：</span>
         <Select
           value={interviewTimeId}
-          defaultActiveFirstOption
           style={{ width: 240 }}
           onChange={(e) => {
             fetchList(e, false);
